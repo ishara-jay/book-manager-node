6a22df301f8906a94f27db2dd38a0263fc8aba50
Ishara Jay <ishara.u.jayaweera@gmail.com>
2025-03-26 23:52:09 +0800

    part 2 and 3 : sql statements

---
+++ Gunawardena_Dulanja_book_createStatements.sql
@@ -0,0 +1,19 @@
+CREATE TABLE book (
+    bookID INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(43) NOT NULL,
+    author VARCHAR(18) NOT NULL,
+    numberOfBooks INT NOT NULL,
+    topic VARCHAR(18) NOT NULL
+);
+
+INSERT INTO book (name, author, numberOfBooks, topic) 
+VALUES ('The Art of Computer Programming', 'Donald Knuth', 5, 'Algorithms');
+
+INSERT INTO book (name, author, numberOfBooks, topic) 
+VALUES ('Clean Code', 'Robert C. Martin', 10, 'Software Eng.');
+
+CREATE USER 'thomas'@'localhost' IDENTIFIED BY 'JPALbMyg';
+
+GRANT ALL PRIVILEGES ON *.* TO 'thomas'@'localhost' WITH GRANT OPTION;
+
+FLUSH PRIVILEGES;
